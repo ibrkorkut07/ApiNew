5790f9d84e24cff4e06c16802362ae307c4cc761
ibrkorkut07 <http://github.com/ibrkorkut07>
2022-11-07 09:50:59 +0100

    Object Mappers

---
+++ src/test/java/ObjectMapperClasses/ReqresRequest.java
@@ -0,0 +1,16 @@
+package ObjectMapperClasses;
+
+import org.junit.Test;
+
+public class ReqresRequest {
+
+    @Test
+    public void test(){
+
+        ReqresToken obje=new ReqresToken();
+        System.out.println(obje.TokenAl());
+
+
+
+    }
+}
